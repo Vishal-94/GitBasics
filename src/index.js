ba0ea00c0d1e41f18e344bf63ee9e5918b7df5bb
Arpit New <arpit_jain@localhost.com>
2022-01-06 12:47:13 +0530

    Revert "Adds calc helper modules"
    This reverts commit 0007afd72a955a66e0b974c745a32a8ca2d11a9a.

--- src/index.js
+++
@@ -1,5 +0,0 @@
-const add = require("./helpers/add");
-const substract = require("./helpers/substract");
-
-console.log(`Addition of 2 + 4 ${add(2, 4)}`)
-console.log(`Substraction of 10 - 4 ${substract(10, 4)}`)
